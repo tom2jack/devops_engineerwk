87bef94bcdc8752d66f8d605f31baece41d4d357
coffee-code <coffee.code88@gmail.com>
2016-04-17 18:12:51 +0700

    commit test

--- python/gross_pay.py
+++ python/gross_pay.py
@@ -4,13 +4,18 @@ import sys
 #Enter Hours: 35
 #Enter Rate: 2.75
 #Pay: 96.25
+
+def computepay (hours, rate):
+    if hours > 40:
+        print "Your gross pay: %.2f$" % (40 * rate + ((hours-40) * rate * 1.5))
+    else:
+        print "Your gross pay: %.2f$" % (hours * rate)
+
 try:
     hours = eval(raw_input("Enter your hours: "))
     rate = eval(raw_input("Enter your rate ($): "))
 except:
     print "Error, please enter a numeric input"
     sys.exit()
-if hours > 40:
-    print "Your gross pay: %.2f$" % (40 * rate + ((hours-40) * rate * 1.5))
-else:
-    print "Your gross pay: %.2f$" % (hours * rate)
+
+computepay(hours, rate)
