50ac5df4e78996e21736fc674907233862b9d4c5
coffee-code <coffee.code88@gmail.com>
2016-04-17 18:03:33 +0700

    commit test

--- python/gross_pay.py
+++ python/gross_pay.py
@@ -1,9 +1,16 @@
+import sys
 #Write a program to prompt the user for hours and
 #rate per hour to compute gross pay.
 #Enter Hours: 35
 #Enter Rate: 2.75
 #Pay: 96.25
-
-hours = eval(raw_input("Enter your hours: "))
-rate = eval(raw_input("Enter your rate ($): "))
-print "Your gross pay: %.2f$" % (hours * rate)
+try:
+    hours = eval(raw_input("Enter your hours: "))
+    rate = eval(raw_input("Enter your rate ($): "))
+except:
+    print "Error, please enter a numeric input"
+    sys.exit()
+if hours > 40:
+    print "Your gross pay: %.2f$" % (40 * rate + ((hours-40) * rate * 1.5))
+else:
+    print "Your gross pay: %.2f$" % (hours * rate)
