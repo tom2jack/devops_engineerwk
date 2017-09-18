9e7aab1eb91a94162724ddc57a67b8ef0080e831
Broundon Seino <broundon.seino@powerhrg.com>
2017-09-18 01:13:05 -0400

    modified

--- python/gross_pay.py
+++ python/gross_pay.py
@@ -6,12 +6,15 @@ import math
 #Enter Hours: 35
 #Enter Rate: 2.75
 #Pay: 96.25
-
 def computepay (hours, rate):
-    if hours > 40:
+  if hours > 40:
         print "Your gross pay: %.2f$" % (40 * rate + ((hours-40) * rate * 1.5))
-    else:
-        print "Your gross pay: %.2f$" % (hours * rate)
+        extra_hours = hours - 40
+        extra_rate = rate * 1.5
+        pay = rate * hours + (extra_rate * extra_hours)
+  else:
+        print "Your gross pay: %.2f$" % (hrs * rt)
+        print "Pay:", pay
 
 try:
     hours = eval(raw_input("Enter your hours: "))
